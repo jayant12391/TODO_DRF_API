a1a41e89f502941628f5a77c30d0f62e231a3590
jayant12391 <jayantdankhade@gmail.com>
2023-10-20 19:10:40 +0530

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render 
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET', 'POST'])
+def apiOverview(request):
+    return JsonResponse('API BASE POINT', SAFE=False)
+
+
+
+
+
+
+
